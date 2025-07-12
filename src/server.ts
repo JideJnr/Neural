51eb4455de33c1ddbef73d2b260d9c3447170f26
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-07-12 19:37:30 +0100

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 const PORT = process.env.PORT || 3000;
 dotenv.config();
-
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
